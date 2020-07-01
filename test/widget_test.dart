cea7361f9d3092bc8bac21e5d7939771a0b9894a
Johann Munoz <johannmunoz.dev@gmail.com>
2020-07-01 14:28:20 +0930

    Test change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,6 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-    
 
     // Verify that our counter starts at 0.
     expect(find.text('Please add a location'), findsOneWidget);
